518039f4321c82d774ee508427f5858c37a3e07b
Sasanga Edirisinghe <madhumaledirisinghe@gmail.com>
2024-08-03 07:19:06 +0530

    added base entity

---
+++ FlightData.Models/Entities/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using EFCore.ModelBuilderExtensions.Attributes;
+
+namespace FlightData.Models.Entities;
+
+public class BaseEntity
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    [SqlDefaultValue("getutcdate()")]
+    public DateTime CreatedAt { get; set; }
+    [SqlDefaultValue("getutcdate")]
+    public DateTime UpdatedAt { get; set; }
+}
---
+++ FlightData.Models/Entities/FlightData.cs
@@ -0,0 +1,7 @@
+﻿namespace FlightData.Models.Entities;
+
+public class FlightData : BaseEntity
+{
+    public string RegistrationNumber { get; set; } = string.Empty;
+
+}
