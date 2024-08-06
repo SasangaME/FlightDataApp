fd560d5d20a47284a7a8bfb10912dfa863a9a6ad
Sasanga Edirisinghe <madhumaledirisinghe@gmail.com>
2024-08-06 10:40:09 +0530

    Merge pull request #3
    * added auto-mapper, location - city migration

--- FlightData.Models/Entities/Location.cs
+++ FlightData.Models/Entities/Location.cs
@@ -5,10 +5,10 @@ namespace FlightData.Models.Entities;
 public class Location : BaseEntity
 {
     [MaxLength(200)]
-    public string Name { get; set; } = string.Empty;
+    public string City { get; set; } = string.Empty;
     [MaxLength(200)]
     public string Country { get; set; } = string.Empty;
 
-    public ICollection<FlightDetails> Origins { get; set; }
-    public ICollection<FlightDetails> Destinations { get; set; }
+    public ICollection<FlightDetails> Origins { get; set; } = new List<FlightDetails>();
+    public ICollection<FlightDetails> Destinations { get; set; } = new List<FlightDetails>();
 }
---
+++ FlightData.Models/Mappings/FlightDataProfile.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+
+namespace FlightData.Models.Mappings;
+
+public class FlightDataProfile : Profile
+{
+    public FlightDataProfile()
+    {
+        // CreateMap<>()
+    }
+}
